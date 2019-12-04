f467533d93251547a3f6f9db2be3362a2f0bbbd6
Ricardo Augusto Fernandes Junior <rafernandesjr@usp.br>
2019-12-03 23:28:29 -0300

    Update Registro e Inicio

--- adot/forms.py
+++ adot/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
 from phonenumber_field.formfields import PhoneNumberField
+from django.utils.safestring import mark_safe
 
 
 class LoginForm(AuthenticationForm):
@@ -15,4 +16,4 @@ class SignUpForm(UserCreationForm):
     confirm_password = forms.CharField(label="Repita a senha:", widget=forms.PasswordInput, help_text='As senhas inseridas devem ser iguais')
     birthdate = forms.DateField(label="Data de Nascimento", widget=forms.SelectDateWidget)
     telephone = PhoneNumberField(label="Celular:")
-    terms_agree = forms.BooleanField(label="Li e aceito os termos de uso")
+    terms_agree = forms.BooleanField(label=mark_safe('Li e aceito os <a href="#" target="_blank">termos de uso</a>'))
